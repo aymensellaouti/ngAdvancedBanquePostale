80fe7f2f14a3cdaced66af95fa59c22e79db6a24
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-23 16:31:32 +0100

    prepare CvModule

--- src/app/todo/todo.module.ts
+++ src/app/todo/todo.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from "@angular/core";
 import { TodoComponent } from "./todo/todo.component";
-import { RouterModule } from "@angular/router";
-import { canLeaveTodoGuard } from "./guards/can-leave-todo.guard";
 import { TodoRoutingModule } from "./todo-routing.module";
 import { FormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
