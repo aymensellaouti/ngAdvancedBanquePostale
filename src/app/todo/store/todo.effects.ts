d3dcfae7382b2977e097f50fa1748a944968925d
aymen2782 <aymen.sellaouti@gmail.com>
2023-10-25 09:24:24 +0100

    effect without dipatch

--- src/app/todo/store/todo.effects.ts
+++ src/app/todo/store/todo.effects.ts
@@ -1,20 +1,33 @@
 import { Injectable } from "@angular/core";
 import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { todoActionGroup } from "./actions";
-import { map, of, switchMap } from "rxjs";
+import { map, of, switchMap, tap } from "rxjs";
 import { Todo } from "../model/todo";
 
 @Injectable({ providedIn: 'root' })
 export class TodoEffect {
-  loadedTodos = createEffect(() => this.actions$.pipe(
-    ofType(todoActionGroup.getTodos),
-    switchMap(
-      action => of([
-          new Todo("1", "first", "doing something"),
-          new Todo("2", "second", "doing another thing"),
+  loadedTodos = createEffect(() =>
+    this.actions$.pipe(
+      ofType(todoActionGroup.getTodos),
+      switchMap((action) =>
+        of([
+          new Todo('1', 'first', 'doing something'),
+          new Todo('2', 'second', 'doing another thing'),
         ])
-    ),
-    map( todos => todoActionGroup.loadedTodos({todos: todos}))
-  ))
+      ),
+      map((todos) => todoActionGroup.loadedTodos({ todos: todos }))
+    )
+  );
+  addTodo = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(todoActionGroup.addTodo),
+        tap((action) => {
+          console.log('Todo added:', action.todo);
+        })
+      ),
+    { dispatch: false }
+  );
+
   constructor(private actions$: Actions) {}
 }
